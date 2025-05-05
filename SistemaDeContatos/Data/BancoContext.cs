d5200bc01967e5beb9b6a53f5ef47d2572f746c0
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-05-05 15:38:22 -0300

    Começando a implementar o banco de dados

--- SistemaDeContatos/Data/BancoContext.cs
+++ SistemaDeContatos/Data/BancoContext.cs
@@ -1,6 +1,13 @@
-﻿namespace SistemaDeContatos.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeContatos.Models;
+
+namespace SistemaDeContatos.Data
 {
-    public class BancoContext
+    public class BancoContext : DbContext
     {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) { 
+        }
+        public DbSet<ContatoModel> Contatos { get; set; }
+
     }
 }
